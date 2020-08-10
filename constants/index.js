3f6c68de1d284f69cfe8f093fdd1de61c3020f5a
Glenn Posadas <hello@glennvon.com>
2020-08-11 00:02:33 +0800

    add constants.

---
+++ constants/index.js
@@ -0,0 +1,74 @@
+/* ==========================================================================
+   Dependencies
+   ========================================================================== */
+
+/* ==========================================================================
+   Public Variables
+   ========================================================================== */
+
+   const STATUS = {
+    SUCCESS: 'success',
+    ERROR: 'error',
+  };
+  
+  const LOGIN_FIELDS = {
+    USERNAME: 'username',
+    PASSWORD: 'password',
+  };
+  
+  const USER_FIELDS = {
+    ID: 'id',
+    FIRSTNAME: 'firstName',
+    LASTNAME: 'lastName',
+    EMAIL: 'email',
+    PASSWORD: 'password',
+  };
+  
+  const NOTE_FIELDS = {
+    TITLE: 'name',
+    CONTENT: 'content',
+    USER_ID: 'user_id',
+    CREATED_AT: 'created_at',
+    UPDATED_AT: 'updated_at',
+  };
+  
+  const DOCUMENT_COLLECTIONS = {
+    USER: 'User',
+    Note: 'Note'
+  };
+  
+  const ELASTICSEARCH_INDICES = {
+    NOTE: 'note'
+  };
+  
+  const RESPONSE_MESSAGES = {
+    MISSING_PARAMETER: (param) => `Missing required parameter/s [${param}]`,
+    INVALID_PARAMETER: (param) => `Invalid parameter/s [${param}]`,
+    DOES_NOT_EXIST_PARAMETER: (param) => `\`${param}\` does not exist`,
+    EXISTING_PARAMETER: (param) => `\`${param}\` already exists`,
+    PARAMETER_CREATED: (param) => `${param} successfully created!`,
+    PARAMETER_UPDATED: (param) => `${param} successfully updated!`,
+    PARAMETER_DELETED: (param) => `${param} successfully deleted!`,
+    UNAUTHORIZED_REQUEST: 'Unauthorized request',
+    TOKEN_EXPIRED: 'Token has expired. Please login again',
+    INVALID_CREDENTIALS: 'Invalid credentials',
+    INVALID_PAYLOAD: 'Invalid payload',
+    LOGIN_SUCCESSFUL: 'Login Successful!',
+    LOGOUT_SUCCESSFUL: 'Logout Successful!',
+    SERVER_ERROR: 'Something went wrong!',
+  };
+  
+  /* ==========================================================================
+     Exports
+     ========================================================================== */
+  
+  module.exports = {
+    STATUS,
+    LOGIN_FIELDS,
+    RESPONSE_MESSAGES,
+    USER_FIELDS,
+    NOTE_FIELDS,
+    DOCUMENT_COLLECTIONS,
+    ELASTICSEARCH_INDICES,
+  };
+  

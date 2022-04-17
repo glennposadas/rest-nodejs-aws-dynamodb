4e5c55381cabf9f6b5c6da26f6d0f76dc75ae151
Glenn Posadas <hello@glennvon.com>
2022-04-17 16:35:13 +0800

    try to add swagger

---
+++ swagger.ts
@@ -0,0 +1,19 @@
+
+export const swaggerDocument = {
+  openapi: '3.0.1',
+  info: {
+      version: '1.0.0',
+      title: 'Glenn Test Rest App',
+      description: 'This is the documentation of the rest app glenn-test.',
+      termsOfService: '',
+      contact: {
+          name: 'Glenn Posadas',
+          email: 'admin@glennvon.com',
+          url: 'https://glennvon.com'
+      },
+      license: {
+          name: 'Apache 2.0',
+          url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
+      }
+  }
+}
